c0d098b7c018f4556d6e19c3d3ca326152a74f8e
Alex Tetervak <tetervak@sheridancollege.ca>
2020-09-22 19:27:23 -0400

    improved Die class and roll logic

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -18,6 +18,7 @@ package com.example.android.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -40,12 +41,10 @@ class MainActivity : AppCompatActivity() {
         // Get the Button view from the layout and assign a click
         // listener to it.
         val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.setOnClickListener { rollDice() }
+        rollButton.setOnClickListener{ rollDice()}
 
         if(savedInstanceState is Bundle){
             die.value = savedInstanceState.getInt("current_die_value")
-        }else{
-            die.roll()
         }
         displayDice()
 
@@ -66,9 +65,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun displayDice() {
-        val randomInt = die.value
-
         val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
+        resultText.text = if(die.value > 0) die.value.toString() else " "
     }
 }
--- app/src/main/java/com/example/android/diceroller/model/Die.kt
+++ app/src/main/java/com/example/android/diceroller/model/Die.kt
@@ -1,6 +1,18 @@
 package com.example.android.diceroller.model
 
-class Die(var value: Int = 1) {
+import java.lang.IllegalArgumentException
+
+class Die(dieValue: Int = 0) {
+
+    // zero means not rolled yet
+    var value: Int = dieValue
+    set(n){
+        if(n in 0..6){
+            field = n
+        }else{
+            throw IllegalArgumentException("Illegal dice value $n")
+        }
+    }
 
     fun roll(){
         value = (1..6).random()
