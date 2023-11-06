9fe057b636137cae008c853f1cd7008a9cbd39b6
Alex Tetervak <tetervak@sheridancollege.ca>
2023-11-06 09:14:46 -0500

    converted build files to kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    id("com.android.application") version "8.1.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+
+    // needed for the dependency injection
+    id("com.google.dagger.hilt.android") version "2.44.2" apply false
+
+    // needed by the local database
+    id("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false
+}
